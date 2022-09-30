4045fadf8a341a8389d228988d19a5a9c2d419f4
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2022-09-30 21:50:53 +0600

    Delete CheatingStrategy.java

--- CheatingStrategy.java
+++
@@ -1,6 +0,0 @@
-public class CheatingStrategy implements ExamPassingStrategy {
-    @Override
-    public void execute() {
-        System.out.println("You have prepared cheat sheets for the exam, your cheat sheets have been noticed,FAIL");
-    }
-}
