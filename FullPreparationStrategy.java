87fc577c3dc90e7e525a94980a23fc29869e5329
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2022-09-30 21:51:13 +0600

    Delete FullPreparationStrategy.java

--- FullPreparationStrategy.java
+++
@@ -1,6 +0,0 @@
-public class FullPreparationStrategy implements ExamPassingStrategy{
-    @Override
-    public void execute() {
-        System.out.println("You have fully prepared for the exam, you have passed Exam!");
-    }
-}
